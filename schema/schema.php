50b2fa5145044ed464fec9a65dd8b099780f68d1
Nathan amudavi <nathanamudavi@gmail.com>
2025-02-04 23:43:12 +0300

    model

--- schema/schema.php
+++ schema/schema.php
@@ -1,12 +1,41 @@
 <?php
+//import dotenv
+use Dotenv\Dotenv;
+
+require_once __DIR__ ."/../vendor/autoload.php";
+$dotenv = Dotenv::createImmutable(__DIR__ .'/..');
+$dotenv->load();
+
+$servername = $_ENV['DB_SERVERNAME'];
+$username = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
+$dbname = $_ENV['DB_NAME'];
+
+// Create connection
+$conn = new mysqli($servername, $username, $password); 
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Create the database if it doesn't exist
+$sql = "CREATE DATABASE IF NOT EXISTS $dbname";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created or exists already.";
+} else {
+    die("Error creating database: " . $conn->error);
+}
+//select database
+$conn->select_db(database: $dbname);
 //tables
 //customers
 $Customer ="CREATE TABLE Customers
 (id INT(6)UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 first_name VARCHAR(30) NOT NULL,
 second_name VARCHAR(30) NOT NULL,
-phone_no VARCHAR(10) NOT NULL,
-pasword VARCHAR(50) NOT NULL,
+phone_no VARCHAR(14) NOT NULL,
+password VARCHAR(50) NOT NULL,
 email VARCHAR(50) NOT NULL,
 reg_date TIMESTAMP DEFAULT CURRENT_TIME ON UPDATE CURRENT_TIMESTAMP";
 //admin
@@ -22,7 +51,8 @@ reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)";
 $Products = "CREATE TABLE Products(
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(20) NOT NULL,
-comany INT(6) NOT NULL,
+company_id INT(6) NOT NULL,
+category_id INT(6) NOT NULL,
 publish_year YEAR,
 price FLOAT(10) NOT NULL,
 stock INT(100) NOT NULL,
@@ -30,9 +60,10 @@ reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)";
 //Company
 $Company = "CREATE TABLE Authors(
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-first_name VARCHAR(30) NOT NULL,
-second_name VARCHAR(30) NOT NULL,
-bio VARCHAR(255))";
+name VARCHAR(20) NOT NULL,
+location VARCHAR(20) NULL,
+phone_no VARCHAR(14) NULL,
+email VARCHAR(50) NULL)";
 //order items
 $Order_items = "CREATE TABLE Order_items(
 id INT(6) UNSIGNED  AUTO_INCREMENT PRIMARY KEY,
@@ -52,8 +83,18 @@ delivery_status VARCHAR(20)
 $Category = "CREATE TABLE Category(
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(20) NOT NULL,
-description VARCHAR(255) NOT NULL,
+description VARCHAR(255) NULL,
 reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)";
 
+// Execute queries
+$conn->query($Customers);
+$conn->query($admin);
+$conn->query($Products);
+$conn->query($Company);
+$conn->query($Order_items);
+$conn->query($Orders);
+$conn->query($Category);
+
+$conn->close();
 
 ?>
