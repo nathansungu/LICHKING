d0e20c093b5d9ff16d1ed107ffbd0caf2bc1c0b9
Nathan amudavi <nathanamudavi@gmail.com>
2025-02-18 22:34:01 +0300

    api function call index.js

--- frontend/index.js
+++ frontend/index.js
@@ -80,7 +80,50 @@ function register() {
         });
     });
 }
+//registeradmin
+function registeradmin() {
+    const first_name = document.getElementById("first_name").value;
+    const second_name = document.getElementById("second_name").value;
+    const email = document.getElementById("email").value;
+    const phone_no = document.getElementById("phone_no").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirm_password").value;
 
+    if (password !== confirmPassword) {
+        showNotification("Passwords do not match!", "adminreg_notification");
+        return;
+    }
+    fetch('http://localhost:3000/admin/register', {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ first_name, second_name, phone_no, email, password })
+    })
+    .then(response => response.json().then(data => ({ status: response.ok, data })) )
+    .then(({ status, data }) => {
+        if (status) {
+            showNotification(data.message, "adminreg_notification");
+        } else {
+            showNotification(data.message, "adminreg_notification");
+            console.error("Error:", data);
+        }
+    })
+}
+//fetch admins
+function fetchadmins(){
+    fetch('http:/localhost:3000/admins',{
+        method: 'post',
+        headers: {"Content-Type": "application/json"},        
+    })
+    .then (response => response.json().then(data => ({ status: response.ok, data })) )
+    .then(({ status, data }) => {
+        if (status) {
+            showNotification(data.message, "adminreg_notification");
+        } else {
+            showNotification(data.message, "adminreg_notification");
+            console.error("Error:", data);
+        }
+    })
+}
 function fetchproducts() {
     let products = [];
     // products.push("Orange");
@@ -244,7 +287,7 @@ function viewcart() {
     .then(data => ({ status: response.ok, data }))
     .then(({ status, data }) => {
         if (status) {
-            showNotification(data.message, "cart_notification");
+            showNotification(data.order, "cart_notification");
         } else {
             showNotification(data.message, "cart_notification");
             console.error("Error:", data);
@@ -273,11 +316,62 @@ function placeorder() {
 // cancel order
 //admin
 function cancelorder() {
-    const order_id = document.getElementById("order_id").value;
-    fetch('http://localhost:3000/order/cancel', {
-        method: 'DELETE',
+    // Get order_id from viewcart function response data
+    viewcart().then(cartData => {
+        const order_id = cartData.data.order.id;
+
+        fetch('http://localhost:3000/order/cancel', {
+            method: 'DELETE',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ order_id })
+        })
+        .then(response => response.json())
+        .then(data => ({ status: response.ok, data }))
+        .then(({ status, data }) => {
+            if (status) {
+                showNotification(data.message, "admin_notification");
+            } else {
+                showNotification(data.message, "admin_notification");
+                console.error("Error:", data);
+            }
+        });
+    });
+}
+
+// customer
+function customercancelorder() {
+    const customer_id = sessionStorage.getItem("customer_id");
+    viewcart().then(cartData => {
+        const order_id = cartData.data.order.id;
+
+        fetch('http://localhost:3000/order/cancel', {
+            method: 'DELETE',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ customer_id, order_id })
+        })
+        .then(response => response.json())
+        .then(data => ({ status: response.ok, data }))
+        .then(({ status, data }) => {
+            if (status) {
+                showNotification(data.message, "cart_notification");
+            } else {
+                showNotification(data.message, "cart_notification");
+                console.error("Error:", data);
+            }
+        });
+    });
+}
+//update delivery status
+function updatedeliverystatus() {
+    const order_id =  viewcart().then(cartData => {
+        let id = cartData.data.order.id;
+        return id;
+    });
+    const status = document.getElementById("status").value;
+    fetch('http://localhost:3000/order/update_delivery_status', {
+        method: 'PUT',
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ order_id })
+        body: JSON.stringify({ order_id, status })
     })
     .then(response => response.json())
     .then(data => ({ status: response.ok, data }))
@@ -290,22 +384,137 @@ function cancelorder() {
         }
     })
 }
+//check delivery status
+function checkdeliverystatus() {
+    const order_id = viewcart().then(cartData => {
+        let id = cartData.data.order.id;
+        return id;
+    });
+    fetch('http://localhost:3000/order/delivery_status', {
+        method: 'GET',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ order_id })
+    })
+    .then(response => response.json())
+    .then(data => ({ status: response.ok, data }))
+    .then(({ status, data }) => {
+        if (status) {
+            showNotification(data.message + status, "cart_notification");
+        } else {
+            showNotification(data.message, "cart_notification");
+            console.error("Error:", data);
+        }
+    })
+}
+//function to add company 
+function addcompany(){
+    const name = document.getElementById("companyname").value;
+    const location = document.getElementById("companylocation").value;
+    const phone_no = document.getElementById("phone_no").value;
+    const email = document.getElementById("email").value;
 
-function showNotification(message, id ) {
+    fetch('http://localhost:3000/company/add', {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name, location, phone_no, email })
+    })
+    .then(response => response.json())
+    .then(data => ({ status: response.ok, data }))
+    .then(({ status, data }) => {
+        if (status) {
+            showNotification(data.message, "company_notification");
+        } else {
+            showNotification(data.message, "company_notification");
+            console.error("Error:", data);
+        }
+    })
+    .catch(error => {
+        showNotification('Error: ' + error.message, "company_notification");
+        console.error('Error:', error);
+    });
+}
+//get all companies
+function getcompanies() {
+    fetch('http://localhost:3000/company', {
+        method: 'GET',
+        headers: { "Content-Type": "application/json" },
+    })
+    .then(response => response.json())
+    .then(data => ({ status: response.ok, data }))
+    .then(({ status, data }) => {
+        if (status) {
+            showNotification(data.message, "company_notification");
+        } else {
+            showNotification(data.message, "company_notification");
+            console.error("Error:", data);
+        }
+    })
+    .catch(error => {
+        showNotification('Error: ' + error.message, "company_notification");
+        console.error('Error:', error);
+    });
+}
+//add category
+function addcategory() {
+    const name = document.getElementById("categoryname").value;
+    fetch('http://localhost:3000/category/add', {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name })
+    })
+    .then(response => response.json())
+    .then(data => ({ status: response.ok, data }))
+    .then(({ status, data }) => {
+        if (status) {
+            showNotification(data.message, "category_notification");
+        } else {
+            showNotification(data.message, "category_notification");
+            console.error("Error:", data);
+        }
+    })
+    .catch(error => {
+        showNotification('Error: ' + error.message, "category_notification");
+        console.error('Error:', error);
+    });
+}
+//get all categories
+function getcategories() {
+    fetch('http://localhost:3000/category', {
+        method: 'GET',
+        headers: { "Content-Type": "application/json" },
+    })
+    .then(response => response.json())
+    .then(data => ({ status: response.ok, data }))
+    .then(({ status, data }) => {
+        if (status) {
+            showNotification(data.message, "category_notification");
+        } else {
+            showNotification(data.message, "category_notification");
+            console.error("Error:", data);
+        }
+    })
+    .catch(error => {
+        showNotification('Error: ' + error.message, "category_notification");
+        console.error('Error:', error);
+    });
+}
+
+function showNotification(message, id) {
     const notification = document.getElementById(id);
     if (notification) {
         notification.style.display = "block";
         notification.innerText = message;
-        //notification.className = notification; 
 
         setTimeout(() => {
             notification.style.display = "none";
-        }, 7000); // Hide the notification after 3 seconds
+        }, 7000); // Hide the notification after 7 seconds
     } else {
         console.error(`Notification element with id '${id}' not found.`);
     }
 }
-//toggle password visibility
+
+
+// toggle password visibility
 function togglePasswordVisibility(id) {
     var passwordField = document.getElementById(id);
     if (passwordField.type === "password") {
